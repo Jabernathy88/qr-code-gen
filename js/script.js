bab820e3db852cd75ea4c5a2f80a5925f274016a
Jeremy Abernathy <j.e.abernathy@gmail.com>
2023-05-23 16:33:53 -0400

    Functions for fetching QR code, clearning UI.

--- js/script.js
+++ js/script.js
@@ -3,11 +3,44 @@ const qr = document.getElementById('qrcode');
 
 const onGenerateSubmit = (e) => { // event parameter
     e.preventDefault();
+    clearUI();
 
-    const url = document.getElementById('url').ariaValueMax;
-    const size = document.getElementById('size').ariaValueMax;
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
 
-    console.log(url, size);
+    if (url === '') {
+        alert('Please enter a URL');
+    } else {
+        showSpinner();
+
+        setTimeOut(() => {
+            hideSpinner();
+
+            generateQRCode(url, size);
+        }, 1000);
+    }
 };
+
+const generateQRCode = (url, size) => {
+    const qrcode = new QRCode('qrcode', {
+        text: url,
+        width: size,
+        height: size,
+    })
+}
+
+const showSpinner = () => {
+    document.getElementById('spinner').style.display = 'block';
+};
+
+const hideSpinner = () => {
+    document.getElementById('spinner').style.display = 'none';
+};
+
+const clearUI = () => {
+    qr.innerHTML = '';
+}
+
+hideSpinner();
 
 form.addEventListener('submit', onGenerateSubmit);
